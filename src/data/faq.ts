9c9be354d90c70cd024b0769b2172644b42086dc
Sebastien Branly <sebranly.pinf@gmail.com>
2023-03-12 01:35:48 +0100

    Quick fix for Building 21 dead drop location (#12)

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -63,7 +63,7 @@ const questionsAnswers: QuestionAnswer[] = [
     map: true,
     question: 'Where are dead drops?',
     answer: [
-      'There are several dead drops per map. For a specific season, they are always at the same spots. However with each new season, their locations change. For this reason, I invite you to take a look at two amazing websites that focus exclusively on locations.'
+      'There are several dead drops per map. For a specific season, they are always at the same spots (except for Building 21 which has locations changing per game). However with each new season, their locations change. For this reason, I invite you to take a look at two amazing websites that focus exclusively on locations.'
     ]
   },
   {
