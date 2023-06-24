0d978aed0de312b4214f0289bf19fad401395717
sebranly <sebranly.pinf@gmail.com>
2023-06-24 19:46:56 +0200

    feat: add sanitize function for api

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -1,5 +1,61 @@
 import { MAX_TIMERS } from '../constants/game';
-import { Timer } from '../types';
+import { APITimer, Timer, TimerFrequency, TimerType } from '../types';
+
+/**
+ * @name sanitizeAPITimers
+ * @description Returns API timers by removing any wrong value
+ */
+const sanitizeAPITimers = (APIResponse: any) => {
+  if (!APIResponse) return [];
+  if (!Array.isArray(APIResponse)) return [];
+
+  const timers: APITimer[] = [];
+
+  APIResponse.forEach((row: any) => {
+    if (!row) return;
+    if (typeof row !== 'object') return;
+
+    // Mandatory fields
+    const { data, title, type } = row;
+    if (typeof title !== 'string' || title === '') return;
+    if (typeof type !== 'string' || !Object.values(TimerType).includes(type as any)) return;
+    if (!data || !Array.isArray(data) || !data.length) return;
+
+    // data has to be fully correct for the overall timer to be accepted
+    if (type === TimerType.Status && data.length < 2) return;
+    if (type !== TimerType.Status && data.length !== 1) return;
+
+    const incorrectData = data.find((dataValue: any) => {
+      const { color, description, time, textOverride } = dataValue;
+
+      if (typeof time !== 'number') return true;
+      if (typeof color !== 'undefined' && typeof color !== 'string') return true;
+      if (typeof description !== 'undefined' && typeof description !== 'string') return true;
+      if (typeof textOverride !== 'undefined') {
+        if (typeof textOverride !== 'object' || Array.isArray(textOverride)) return true;
+        const { subtitle, title } = textOverride;
+
+        if (typeof subtitle !== 'undefined' && typeof subtitle !== 'string') return true;
+        if (typeof title !== 'undefined' && typeof title !== 'string') return true;
+      }
+
+      return false;
+    });
+
+    if (incorrectData) return;
+
+    // Optional fields
+    const { frequency, showPostEvent, subtitle, subtitlePostEvent } = row;
+    if (typeof frequency !== 'undefined' && !Object.values(TimerFrequency).includes(frequency)) return;
+    if (typeof showPostEvent !== 'undefined' && typeof showPostEvent !== 'boolean') return;
+    if (typeof subtitle !== 'undefined' && typeof subtitle !== 'string') return;
+    if (typeof subtitlePostEvent !== 'undefined' && typeof subtitlePostEvent !== 'string') return;
+
+    timers.push(row);
+  });
+
+  return timers;
+};
 
 /**
  * @name sanitizeTimersCookie
@@ -42,4 +98,4 @@ const sanitizeTimersCookie = (cookieValue: any, maxTimers = MAX_TIMERS) => {
   return timers;
 };
 
-export { sanitizeTimersCookie };
+export { sanitizeAPITimers, sanitizeTimersCookie };
