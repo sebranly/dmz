6fb6adfe8f89fc2d70917f02aa68fc610d970141
Sebastien Branly <sebranly.pinf@gmail.com>
2023-07-30 13:14:33 +0200

    Eslint, Prettier and Husky (#24)

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -70,14 +70,14 @@ const questionsAnswers: QuestionAnswer[] = [
     question: 'What about items?',
     answer: [
       'Items sometimes have a displayed value underneath (in your backpack) which corresponds to the equivalent amount of money they give you when exfiltrating or deposited into dead drops.',
-      `Some other items don't have a displayed value but one exists anyway. We don't have a definite list but for instance depositing a weapon into dead drops will award you <span class='text-lime-500'>${formatMoney(
+      `Some other items don’t have a displayed value but one exists anyway. We don’t have a definite list but for instance depositing a weapon into dead drops will award you <span class='text-lime-500'>${formatMoney(
         WEAPON_VALUE,
         true
       )}</span> (which is then converted for time reduction). The number of attachments does not matter.`
     ]
   },
   {
-    question: "I don't understand the use-case(s)",
+    question: 'I don’t understand the use-case(s)',
     answer: [
       `Once a timer is set up on the website, it decreases on the website every second like the in-game cooldown timer. However, the latter is only visible on the game menu. So, after some time spent fighting into DMZ, <b>this website allows you to accurately know how much time is left and, more importantly, how to split money within your squad in order not to waste any dollar</b>.`,
       `Everyone may be able to get their insured weapons back. You can then use the remaining money you saved for buying better things.`,
@@ -91,7 +91,7 @@ const questionsAnswers: QuestionAnswer[] = [
       `The formulae have been verified several times for all seasons including the latest, <span class='text-amber-500'>Season ${displaySeason(
         CURRENT_SEASON
       )}</span>.`,
-      `The game is known to have multiple bugs affecting how the backpack total is being displayed (e.g. some items don't count in the total up until the end result screen). A bug happening on the result screen, is dog tags counting for time reduction, but not being reflected in the total money being exfiltrated with. Each <span class='text-amber-700'>bronze</span>, <span class='text-slate-500'>silver</span>, <span class='text-amber-300'>gold</span> and <span class='text-indigo-400'>damascus</span> dog tag awards you <span class='text-amber-700'>${formatMoney(
+      `The game is known to have multiple bugs affecting how the backpack total is being displayed (e.g. some items don’t count in the total up until the end result screen). A bug happening on the result screen, is dog tags counting for time reduction, but not being reflected in the total money being exfiltrated with. Each <span class='text-amber-700'>bronze</span>, <span class='text-slate-500'>silver</span>, <span class='text-amber-300'>gold</span> and <span class='text-indigo-400'>damascus</span> dog tag awards you <span class='text-amber-700'>${formatMoney(
         DOG_TAG_VALUES['bronze'],
         true
       )}</span>, <span class='text-slate-500'>${formatMoney(
@@ -120,8 +120,8 @@ const questionsAnswers: QuestionAnswer[] = [
       '<b>They are now respectively 1, 2 and 3 hours.</b>',
       'The locations of dead drops in Al Mazrah and Building 21 have changed in Season 02.',
       'A new map called Ashika Island is available since Season 02.',
-      `The Access Card for Building 21 is now single-use since Season 02 (since Season 02 Reloaded, it doesn't have to be equipped in the backpack anymore though).`,
-      `In Season 03, lots of changes happened including the introduction of Bartering, Contraband Workbenches, New Backpacks, New Plate Carriers as well as Active Duty Operator Slots. However, for the purpose of this website, most updates don't have an influence, as Contraband Weapons are shared between Active Duty Operators.`,
+      `The Access Card for Building 21 is now single-use since Season 02 (since Season 02 Reloaded, it doesn’t have to be equipped in the backpack anymore though).`,
+      `In Season 03, lots of changes happened including the introduction of Bartering, Contraband Workbenches, New Backpacks, New Plate Carriers as well as Active Duty Operator Slots. However, for the purpose of this website, most updates don’t have an influence, as Contraband Weapons are shared between Active Duty Operators.`,
       `However, some bundles (that need to be purchased) give the advantage of using a weapon with its own <b>${BUNDLE_TIMER_MIN}-min cooldown timer</b> (no matter the insured slot), so we updated the website to reflect that (in Quick Options section).`,
       `In Season 03 Reloaded, a new Exclusion Zone called Koschei Complex has been introduced. It is available exclusively from Al Mazrah.`,
       `The glitch about heartbeat sensors awarding lots of time reduction has been patched (unfortunately).`,
@@ -150,13 +150,13 @@ const questionsAnswers: QuestionAnswer[] = [
   {
     question: 'Can I bake cakes with this website?',
     answer: [
-      `Sure, it's a countdown timer at its core after all. Pro-tip: just make sure your cake does not take longer than ${MAX_HOURS_FOR_TIMER} hours.`
+      `Sure, it’s a countdown timer at its core after all. Pro-tip: just make sure your cake does not take longer than ${MAX_HOURS_FOR_TIMER} hours.`
     ]
   },
   {
     question: 'What about cookies?',
     answer: [
-      `On this website we only use one browser cookie called <i><code>${COOKIE_TIMERS}</code></i>. We do this in order to save the timers that you created, so that upon refreshing the page you don't lose them.`
+      `On this website we only use one browser cookie called <i><code>${COOKIE_TIMERS}</code></i>. We do this in order to save the timers that you created, so that upon refreshing the page you don’t lose them.`
     ]
   }
 ];
